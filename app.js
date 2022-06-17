ba9e1e18498bc31baafaf011249ddc509f3f82d1
Mert Helvaci <mert.helvaci2@getir.com>
2022-06-17 16:25:28 +0300

    prepare app for deployment

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
+const compression = require("compression");
 
 const setHeaders = require("./middleware/setHeaders");
 const errorMiddleware = require("./middleware/error");
@@ -12,6 +14,8 @@ const watchlistRoutes = require("./routes/watchlist");
 
 const app = express();
 
+app.use(helmet());
+app.use(compression());
 app.use(bodyParser.json());
 app.use(setHeaders);
 
@@ -22,9 +26,9 @@ app.use(errorMiddleware);
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.9r7m1gx.mongodb.net/moviedb?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.9r7m1gx.mongodb.net/${process.env.MONGODB_DEFAULT_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(4000, () => console.log("Server listening on port 4000..."));
+    app.listen(process.env.PORT || 4000);
   })
   .catch((err) => console.log(err));
